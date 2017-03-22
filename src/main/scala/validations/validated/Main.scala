0c1a779b58ee7e6ebf5c2e2fbda60caa33434ad1
Mike Gehard <mike@wundercapital.com>
2017-03-22 14:39:26 -0400

    Make validated flow a bit better

--- src/main/scala/validations/validated/Main.scala
+++ src/main/scala/validations/validated/Main.scala
@@ -3,8 +3,53 @@ package validations.validated
 import java.time.LocalDateTime
 
 import cats.data.Validated.{invalidNel, valid}
-import cats.data.{NonEmptyList, Validated, ValidatedNel}
+import cats.data.{NonEmptyList, Validated}
 import cats.implicits._
+
+class Car(name: String, modelYear: Int) {
+  override def toString: String =
+    s"Car: name=$name, model year=$modelYear"
+}
+
+object Car {
+  // This type signature now tells you that a non-empty list of CarErrors or a car is returned.
+  // This is similar to Either[List[CarError], Car] but has easy ways accumulate errors that you would have
+  // to write yourself if you used Either[List[CarError], Car]
+  def apply(name: String, modelYear: Int): Validated[NonEmptyList[CarError], Car] = {
+    // Combine the validated fields into the car
+    // Instead of forming a monad to chain results,
+    // Validated is an applicative functor that allows us to
+    // consolidate all of the errors or the successful results
+    // The success case is a tuple with the valid values
+    // or the failure case is a Non-Empty list of CarErrors.
+    (
+      validateName(name)
+        |@| validateModelYear(modelYear)
+      ).map(
+      (name, year) => new Car(name, year)
+    )
+  }
+
+  private def validateName(value: String): Validated[NonEmptyList[CarError], String] =
+    if (invalidName(value)) {
+      invalidNel(new NameEmptyError)
+    } else {
+      valid(value)
+    }
+
+  private def validateModelYear(value: Int): Validated[NonEmptyList[CarError], Int] =
+    if (invalidModelYear(value)) {
+      invalidNel(new ModelYearOutsideValidRangeError)
+    } else {
+      valid(value)
+    }
+
+  private def invalidName(value: String): Boolean =
+    value.isEmpty
+
+  private def invalidModelYear(value: Int): Boolean =
+    value < 1900 || value > LocalDateTime.now().getYear
+}
 
 /** ******** Errors *****************/
 sealed trait CarError {
@@ -19,40 +64,6 @@ class NameEmptyError extends CarError {
 class ModelYearOutsideValidRangeError extends CarError {
   override val message: String =
     "Invalid car model name"
-}
-
-class Car(name: String, modelYear: Int) {
-  override def toString: String =
-    s"Car: name=$name, model year=$modelYear"
-}
-
-/** ******** Domain objects *****************/
-object Car {
-  // This type signature now tells you that a non-empty list of CarErrors or a car is returned.
-  // This is similar to Either[List[CarError], Car] but has easy ways accumulate errors that you would have
-  // to write yourself if you used Either[List[CarError], Car]
-  def apply(name: String, modelYear: Int): Validated[NonEmptyList[CarError],Car] = {
-    val nameResult: ValidatedNel[CarError, String] = if (name.isEmpty) {
-      invalidNel(new NameEmptyError)
-    } else {
-      valid(name)
-    }
-    val yearResult: ValidatedNel[CarError, Int] = if (modelYear < 1900 || modelYear > LocalDateTime.now().getYear) {
-      invalidNel(new ModelYearOutsideValidRangeError)
-    } else {
-      valid(modelYear)
-    }
-
-    // Combine the validated fields into the car
-    // Instead of forming a monad to chain results,
-    // Validated is an applicative functor that allows us to
-    // consolidate all of the errors or the successful results
-    // Either the success case is a tuple with the valid values
-    // or the failure case is a Non-Empty list of CarErrors.
-    (nameResult |@| yearResult).map(
-      (name, year) => new Car(name, year)
-    )
-  }
 }
 
 /** ******** Main program *****************/
